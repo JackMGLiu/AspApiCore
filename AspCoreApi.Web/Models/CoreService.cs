1d95337987d5a4c10a33f6a6303d7a0d73f4dcea
Jack-Liu <love_zs1314@126.com>
2018-08-21 17:11:53 +0800

    完善代码

---
+++ AspCoreApi.Web/Models/CoreService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspCoreApi.Infrastructure.Core.Interface;
+using AutoMapper;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+
+namespace AspCoreApi.Web.Models
+{
+    public class CoreService<T> : ICoreService<T>
+    {
+        public IUnitOfWork UnitOfWork { get; }
+        public ILogger<T> Logger { get; }
+        public IMapper Mapper { get; }
+
+        public IFileProvider FileProvider => throw new NotImplementedException();
+
+        public CoreService(
+            IUnitOfWork unitOfWork,
+            ILogger<T> logger,
+            IMapper mapper)
+        {
+            UnitOfWork = unitOfWork;
+            Logger = logger;
+            Mapper = mapper;
+        }
+
+        public virtual void Dispose()
+        {
+            UnitOfWork?.Dispose();
+        }
+    }
+}
--- AspCoreApi.Web/Startup.cs
+++ AspCoreApi.Web/Startup.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspCoreApi.DataContext.Data;
+using AspCoreApi.Infrastructure.Core.Interface;
 using AspCoreApi.Repositories.Angular;
 using AspCoreApi.Repositories.Angular.Impl;
+using AspCoreApi.Web.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
 
 namespace AspCoreApi.Web
 {
@@ -26,6 +30,10 @@ namespace AspCoreApi.Web
             services.AddMvc();
 
             services.AddScoped<IClientRepository, ClientRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped(typeof(ICoreService<>), typeof(CoreService<>));
+
+            services.AddAutoMapper();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
